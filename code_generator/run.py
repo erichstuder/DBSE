c883e52d2bf13360da9f3bf59e759784f0a59596
erichstuder <richu_@hotmail.com>
2026-01-02 15:50:47 +0100

    automate tests

---
+++ code_generator/run.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import sys
+import pathlib
+
+sys.path.append(str(pathlib.Path(__file__).parent.parent / 'project_management'))
+from executor import Executor # type: ignore
+
+
+if __name__ == "__main__":
+    additional_arguments = [
+        {
+            'flag': '-t',
+            'name': '--test',
+            'help': 'Run tests.'
+        },
+    ]
+
+    ex = Executor(additional_arguments, description='Execute feature tests')
+
+    if ex.arguments.test:
+        commands = (
+            './run-tests.sh'
+        )
+    else:
+        commands = None
+
+    ex.run(commands)
--- run.py
+++ run.py
@@ -5,6 +5,7 @@ from project_management.dispatcher import run_dispatcher
 if __name__ == "__main__":
     scripts = {
         'extension': 'extension/run.py',
+        'code_generator': 'code_generator/run.py',
     }
 
     run_dispatcher(scripts)
